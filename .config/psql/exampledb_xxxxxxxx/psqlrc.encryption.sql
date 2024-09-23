fc1f6b699e98e044c988cfeb64ff5b81f6064c54
AJ ONeal <aj@therootcompany.com>
2024-09-22 23:44:07 -0600

    feat(psql): various session param & encryption examples

--- .config/psql/exampledb_xxxxxxxx/psqlrc.encryption.sql
+++ .config/psql/exampledb_xxxxxxxx/psqlrc.encryption.sql
@@ -45,8 +45,33 @@ FROM
 -- raw encrypt / decrypt example
 -- https://www.postgresql.org/docs/current/pgcrypto.html#PGCRYPTO-RAW-ENC-FUNCS
 
+WITH "aes_key_cte" AS (
+    SELECT
+        current_setting('my.aes_128_key')::bytea AS "aes_key",
+        'sensitive data (raw)' AS "plain_original"
+),
+"raw_example_table_cte" AS (
+    SELECT
+        "aes_key",
+        "plain_original",
+        encrypt(convert_to("plain_original", 'UTF8'), "aes_key", 'aes') AS "raw_enc_column"
+    FROM
+        "aes_key_cte"
+)
+SELECT
+    "plain_original",
+    "raw_enc_column",
+    convert_from(decrypt("raw_enc_column", "aes_key", 'aes'), 'UTF8') AS "plain_decrypted"
+FROM
+    "raw_example_table_cte"
+\gx
+
+
 -- convenience functions to reduce boilerplate within queries themselves
-CREATE OR REPLACE FUNCTION encrypt_aes_cbc(plain_text text, aes_key bytea)
+-- note: these are all temporary, but they could all be permanent
+
+-- DROP FUNCTION IF EXISTS "pg_encrypt_text" (text,bytea);
+CREATE OR REPLACE FUNCTION "pg_temp".pg_encrypt_text(plain_text text, aes_key bytea)
 RETURNS bytea
 LANGUAGE sql
 IMMUTABLE
@@ -58,7 +83,8 @@ AS $$
     SELECT encrypt(convert_to(plain_text, 'UTF8'), aes_key, 'aes')
 $$;
 
-CREATE OR REPLACE FUNCTION decrypt_aes_cbc(encrypted_data bytea, aes_key bytea)
+-- DROP FUNCTION IF EXISTS "pg_decrypt_text" (bytea,bytea);
+CREATE OR REPLACE FUNCTION "pg_temp".pg_decrypt_text(encrypted_data bytea, aes_key bytea)
 RETURNS text
 LANGUAGE sql
 IMMUTABLE
@@ -70,23 +96,81 @@ AS $$
     SELECT convert_from(decrypt(encrypted_data, aes_key, 'aes'), 'UTF8')
 $$;
 
-WITH "raw_example_table_cte" AS (
-    SELECT
-        'sensitive data (raw)' AS "plain_original",
-        encrypt_aes_cbc(
-            'sensitive data (raw)',
-            current_setting('my.aes_128_key')::bytea
-        ) AS "raw_enc_column"
+-- wraps the key in a function so that the ::bytea cast only happens once per query
+-- DROP FUNCTION IF EXISTS "my_key" ();
+CREATE OR REPLACE FUNCTION "pg_temp".my_key()
+RETURNS bytea
+LANGUAGE sql
+STABLE -- relies on GUC (the key is different between client sessions), per per-query permanent
+PARALLEL SAFE
+AS $$
+    -- the ::bytea cast breaks the STABLE caching of current_setting('my.foo')
+    SELECT current_setting('my.aes_128_key')::bytea
+$$;
+
+-- conveniently wraps the key and encryption to be efficient across whole queries
+-- DROP FUNCTION IF EXISTS "my_encrypt" (text);
+CREATE OR REPLACE FUNCTION "pg_temp".my_encrypt(text)
+RETURNS bytea
+LANGUAGE sql
+STABLE -- uses my_key() (GUC), but optimizes away the ::bytea cast per query
+PARALLEL SAFE
+AS $$
+    SELECT encrypt(convert_to($1, 'UTF8'), "pg_temp".my_key(), 'aes')
+$$;
+
+-- conveniently wraps the key and decryption to be efficient across whole queries
+-- DROP FUNCTION IF EXISTS "my_decrypt" (text);
+CREATE OR REPLACE FUNCTION "pg_temp".my_decrypt(bytea)
+RETURNS text
+LANGUAGE sql
+STABLE -- uses my_key() (GUC), but optimizes away the ::bytea cast per query
+PARALLEL SAFE
+AS $$
+    SELECT convert_from(decrypt($1, "pg_temp".my_key(), 'aes'), 'UTF8')
+$$;
+
+--
+-- Examples Using the Convenience Functions and Views
+--
+
+CREATE TEMPORARY TABLE IF NOT EXISTS "encrypted_pii_table" (
+    "number" SERIAL PRIMARY KEY,
+    "first_name" VARCHAR(255),
+    "$last_name" BYTEA
+);
+INSERT INTO "encrypted_pii_table" ("first_name", "$last_name")
+VALUES
+    ('John', session_encrypt('Doe')),
+    ('Jane', session_encrypt('Doe')),
+    ('Bob', session_encrypt('Ross'));
+
+CREATE OR REPLACE TEMPORARY VIEW "decrypted_pii_view" AS
+WITH "aes_key" AS (
+    -- just to show the use of a CTE for the key, the optimizations of my_key()
+    -- and my_encrypt() should be the same once query-planned
+    SELECT current_setting('my.aes_128_key')::bytea AS "aes_key"
 )
 SELECT
-    "plain_original",
-    decrypt_aes_cbc(
-        "raw_enc_column",
-        current_setting('my.aes_128_key')::bytea
-    ) AS "plain_decrypted",
-    "raw_enc_column"
+    "number",
+    "first_name",
+    "pg_temp".pg_decrypt_text("$last_name", "aes_key") AS "last_name"
 FROM
-    "raw_example_table_cte" \gx
+    "encrypted_pii_table"
+    CROSS JOIN "aes_key"
+;
+
+SELECT * FROM "encrypted_pii_table";
+SELECT * FROM "decrypted_pii_view";
+
+\echo '==================================================='
+\echo '=              Try it for yourself!               ='
+\echo '==================================================='
+\echo ''
+\echo 'SELECT * FROM "encrypted_pii_table";'
+\echo 'SELECT * FROM "decrypted_pii_view";'
+\echo 'SELECT "first_name", pg_temp.my_decrypt("$last_name") AS "last_name" FROM "encrypted_pii_table";'
+\echo ''
 
 \echo ''
 \echo '===================================================='
